0ca41d2f1be21ac4cc5015a37a0ffe6272e7985a
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-13 15:38:25 -0400

    initialize routes and some middleware

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,17 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 8001;
 const DB = process.env.MONGODB_URI;
+const isProduction = process.env.NODE_ENV === "production";
 
 // Initialize app
 const app = express();
 
 // Connect to MongoDB
+// TODO: add to config folder
 mongoose.connect(DB);
 const db = mongoose.connection;
 
@@ -35,6 +38,32 @@ app.use(express.json());
 // Test route
 app.get("/", (req, res) => {
   res.send("Sonex Backend Server Up and Running!");
+});
+
+app.use(routes);
+
+// Error handling
+app.use((err, _req, res) => {
+  console.log(err);
+  err.status = err.status || 500;
+  err.title = err.title || "Server Error";
+  err.message = err.message || "Something went wrong. Internal server error.";
+  err.errors = err.errors || { server: [err.message] };
+
+  const error = {
+    title: err.title,
+    errors: err.errors,
+    status: err.status,
+    message: err.message,
+  };
+
+  if (isProduction) {
+    res.status(err.status).json(error);
+  } else {
+    // Add the stack trace in development and debug mode
+    error.stack = err.stack;
+    res.json(error);
+  }
 });
 
 // Start server
---
+++ backend/utils/auth.js
@@ -0,0 +1,4 @@
+// Authentication middlewares to check if user is logged in
+// and authorized
+
+// TODO: set up authentication middleware
