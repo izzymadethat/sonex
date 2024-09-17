7625c56a1cba96926763c34e5526dfe0fd595cf6
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-17 07:20:49 -0400

    remove session router (handled by auth)

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const sessionRouter = require("./session");
 const authRouter = require("./auth");
 const projectsRouter = require("./projects");
 
-router.use("/session", sessionRouter);
 router.use("/auth", authRouter);
 router.use("/projects", projectsRouter);
 
