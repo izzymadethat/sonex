3382d576937ff762937e8d1c8564b9390e1640e0
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-24 12:39:41 -0400

    handle project validations

--- backend/routes/api/projects.js
+++ backend/routes/api/projects.js
@@ -1,12 +1,30 @@
 // All project CRUD operations require USER Auth not CLIENT auth
 // Routes start with /api/projects
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 const Client = require("../../models/client");
 const Project = require("../../models/project");
-const { checkIfAuthenticated } = require("../../utils/auth");
+const { authenticatedUsersOnly } = require("../../utils/auth");
+const handleValidationErrors = require("../../utils/validation");
 
-const authenticatedUsersOnly = [checkIfAuthenticated, checkIfUserNotClient];
+// Validation middleware
+const validateProjectInput = [
+  check("title")
+    .exists({ checkFalsy: true })
+    .withMessage("Please enter a title")
+    .isLength({ max: 50 })
+    .withMessage("Title must be less than 50 characters"),
+  check("description")
+    .optional()
+    .isLength({ min: 3, max: 250 })
+    .withMessage("Description must be between 3 and 250 characters"),
+  check("projectAmount")
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage("Please enter a valid amount"),
+  handleValidationErrors,
+];
 
 // Get projects for user
 // GET /api/projects
@@ -19,30 +37,35 @@ router.get("/", authenticatedUsersOnly, async (req, res, next) => {
 // Create a project
 // POST /api/projects
 // user must be logged in
-router.post("/", authenticatedUsersOnly, async (req, res, next) => {
-  const userId = req.user.id;
-  const { title, description, projectAmount } = req.body;
+router.post(
+  "/",
+  authenticatedUsersOnly,
+  validateProjectInput,
+  async (req, res, next) => {
+    const userId = req.user.id;
+    const { title, description, projectAmount } = req.body;
 
-  // if (!title) {
-  //   return res.status(400).json({
-  //     errorMessage: "Please enter a title",
-  //   });
-  // }
+    // if (!title) {
+    //   return res.status(400).json({
+    //     errorMessage: "Please enter a title",
+    //   });
+    // }
 
-  try {
-    const newProject = await new Project({
-      title,
-      description: description || null,
-      userId,
-      projectAmount: projectAmount || 0,
-      paymentStatus: projectAmount ? "unpaid" : "no-charge",
-    }).save();
+    try {
+      const newProject = await new Project({
+        title,
+        description: description || null,
+        userId,
+        projectAmount: projectAmount || 0,
+        paymentStatus: projectAmount ? "unpaid" : "no-charge",
+      }).save();
 
-    res.status(201).json(newProject);
-  } catch (error) {
-    next(error);
+      res.status(201).json(newProject);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 // Get a single project
 // GET /api/projects/:projectId
@@ -106,93 +129,127 @@ router.get(
 // PUT /api/projects/:projectId
 // user must be logged in
 // project must be owned by user
-router.put("/:projectId", authenticatedUsersOnly, async (req, res, next) => {
-  const projectId = req.params.projectId;
-  const userId = req.user.id.toString();
+router.put(
+  "/:projectId",
+  authenticatedUsersOnly,
+  validateProjectInput,
+  async (req, res, next) => {
+    const projectId = req.params.projectId;
+    const userId = req.user.id.toString();
 
-  const {
-    title,
-    description,
-    status,
-    projectAmount,
-    amountPaid,
-    paymentStatus,
-  } = req.body;
+    const {
+      title,
+      description,
+      status,
+      projectAmount,
+      amountPaid,
+      paymentStatus,
+    } = req.body;
 
-  try {
-    const existingProject = await Project.findById(projectId);
+    try {
+      const existingProject = await Project.findById(projectId);
 
-    if (!existingProject) {
-      return res.status(404).json({
-        message: "Project not found ",
-      });
-    }
+      if (!existingProject) {
+        return res.status(404).json({
+          message: "Project not found ",
+        });
+      }
 
-    // check if project belongs to user
-    if (existingProject.userId.toString() !== userId) {
-      return res.status(403).json({
-        message: "You do not have permission to update this project",
-      });
-    }
+      // check if project belongs to user
+      if (existingProject.userId.toString() !== userId) {
+        return res.status(403).json({
+          message: "You do not have permission to update this project",
+        });
+      }
 
-    existingProject.title = title || existingProject.title;
-    existingProject.description = description || existingProject.description;
-    existingProject.status = status || existingProject.status;
-    existingProject.projectAmount =
-      projectAmount || existingProject.projectAmount;
+      existingProject.title = title || existingProject.title;
+      existingProject.description = description || existingProject.description;
+      existingProject.status = status || existingProject.status;
+      existingProject.projectAmount =
+        projectAmount === 0 || projectAmount
+          ? projectAmount
+          : existingProject.projectAmount;
 
-    let messages = {};
+      let messages = {};
 
-    if (!amountPaid) {
-      existingProject.amountPaid = existingProject.amountPaid || 0;
-    }
-    // update payment status
-    if (amountPaid && amountPaid > 0) {
-      existingProject.amountPaid = Number(amountPaid);
+      if (!amountPaid) {
+        existingProject.amountPaid = existingProject.amountPaid || 0;
+      }
+      // update payment status
+      // if the project amount was changed to 0, set the payment status to no-charge
+      if (projectAmount === 0) {
+        // If project amount is 0, mark as 'no-charge'
+        existingProject.paymentStatus = "no-charge";
+      } else {
+        // Handle other payment statuses if projectAmount > 0
+        existingProject.projectAmount = projectAmount;
+
+        // Reassess the payment status based on the current amountPaid
+        if (existingProject.amountPaid > existingProject.projectAmount) {
+          existingProject.paymentStatus = "overpaid";
+        } else if (
+          existingProject.amountPaid === existingProject.projectAmount
+        ) {
+          existingProject.paymentStatus = "paid";
+        } else if (existingProject.amountPaid > 0) {
+          existingProject.paymentStatus = "partially-paid";
+        } else {
+          existingProject.paymentStatus = "unpaid"; // Handles cases where amountPaid is 0 or undefined
+        }
+      }
+
+      // If a new amountPaid is provided in the request, process it
+      if (amountPaid && amountPaid > 0) {
+        existingProject.amountPaid = Number(amountPaid);
+
+        // Re-evaluate the payment status based on the new amountPaid
+        if (existingProject.amountPaid > existingProject.projectAmount) {
+          existingProject.paymentStatus = "overpaid";
+        } else if (
+          existingProject.amountPaid === existingProject.projectAmount
+        ) {
+          existingProject.paymentStatus = "paid";
+        } else {
+          existingProject.paymentStatus = "partially-paid";
+        }
+      }
 
       if (existingProject.amountPaid > existingProject.projectAmount) {
-        existingProject.paymentStatus = "overpaid";
-      } else if (existingProject.amountPaid === existingProject.projectAmount) {
-        existingProject.paymentStatus = "paid";
-      } else {
-        existingProject.paymentStatus = "partially-paid";
+        messages["Overpayment amount warning"] =
+          "Total amount paid is greater than project amount";
       }
+
+      const updatedProject = await existingProject.save();
+
+      const results = {
+        ...updatedProject._doc,
+        messages,
+      };
+
+      res.status(200).json(results);
+    } catch (error) {
+      next(error);
     }
-
-    if (existingProject.amountPaid > existingProject.projectAmount) {
-      messages["Overpayment amount warning"] =
-        "Total amount paid is greater than project amount";
-    }
-
-    const updatedProject = await existingProject.save();
-
-    const results = {
-      ...updatedProject._doc,
-      messages,
-    };
-
-    res.status(200).json(results);
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 // Add a client to a project
 // PUT /api/projects/:projectId/clients
 // project must be owned by user
+// client must already exist for user
 router.put(
   "/:projectId/clients",
   authenticatedUsersOnly,
   async (req, res, next) => {
     const projectId = req.params.projectId;
     const userId = req.user.id.toString();
-    const { clientId } = req.body;
+    const { clientId } = req.query;
 
     try {
       const existingProject = await Project.findById(projectId);
       if (!existingProject) {
         return res.status(404).json({
-          message: "Project not found ",
+          message: "Project not found",
         });
       }
       if (existingProject.userId.toString() !== userId) {
@@ -203,7 +260,14 @@ router.put(
       const existingClient = await Client.findById(clientId);
       if (!existingClient) {
         return res.status(404).json({
-          message: "Client not found ",
+          message: "Client not found",
+        });
+      }
+
+      // Check if the user is in the client's users array
+      if (!existingClient.users.includes(userId)) {
+        return res.status(403).json({
+          message: "This client is not associated with your account",
         });
       }
 
@@ -229,7 +293,7 @@ router.delete("/:projectId", authenticatedUsersOnly, async (req, res, next) => {
 
     if (!existingProject) {
       return res.status(404).json({
-        message: "Project not found ",
+        message: "Project not found",
       });
     }
 
@@ -250,16 +314,5 @@ router.delete("/:projectId", authenticatedUsersOnly, async (req, res, next) => {
     next(error);
   }
 });
-
-function checkIfUserNotClient(req, _res, next) {
-  if (req.client || !req.user) {
-    const error = new Error("Unauthorized");
-    error.status = 403;
-    error.message = "Only authenticated users can access this route";
-    return next(error);
-  }
-
-  next();
-}
 
 module.exports = router;
