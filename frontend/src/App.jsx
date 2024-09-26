0d5ccc729ab1c3260d8dcc63402f4f796a133675
Isaiah V. <isaiah.vickers@outlook.com>
2024-09-26 00:16:26 -0400

    update browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,19 @@
-import { Button } from "@nextui-org/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import Signup from "./pages/Signup";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Button>Button</Button>
-    },
-    {
-      path: "/login",
-      element: <Login />
-    },
-    {
-      path: "/register",
-      element: <Signup />
-    },
-    {
-      path: "/logout",
-      element: <Logout />
-    }
-  ]);
   return (
     <>
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1 className="text-4xl">Home Page</h1>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </Router>
     </>
   );
 }
