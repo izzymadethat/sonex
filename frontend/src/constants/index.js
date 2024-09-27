e99bb89149c5b7d8aca3abfdd2f8862089694d09
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-27 13:46:02 -0400

    build pricing plans section

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,37 @@
+export const pricingPlans = [
+  {
+    id: 1,
+    name: "The SonExperience",
+    price: 15,
+    extraStoragePrice: 7,
+    storage: "256GB",
+    features: [
+      "Unlimited projects and clients",
+      "Unlimited files and uploads",
+      "Projects and payments management*",
+      "Lossless audio streaming",
+      "Free service updates and access to email support",
+      "Up to 256GB Storage Included"
+    ],
+    extras: [
+      "$7/mo for every additional 256GB of storage added to your monthly plan",
+      "No hidden fees! Upgrade storage as needed while keeping all other services unlimited"
+    ],
+    link: "#"
+  },
+  {
+    id: 2,
+    name: "The SonExsphere",
+    price: 0,
+    storage: null,
+    features: [
+      "Eliminate monthly billing",
+      "Host your own Projects and Clients",
+      "Build from our most current source code",
+      "Add your own storage and services",
+      "Not managed by Sonex"
+    ],
+    extras: null,
+    link: "#"
+  }
+];
