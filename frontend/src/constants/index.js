e9cb4d6d647b0af744d1c4df9d102a0fa2d67eef
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-29 13:45:36 -0400

    build marketed users section

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,3 +1,17 @@
+export const navLinks = [
+  {
+    title: "Product",
+    route: "#",
+  },
+  {
+    title: "Pricing",
+    route: "#",
+  },
+  {
+    title: "Blog",
+    route: "#",
+  },
+];
 export const pricingPlans = [
   {
     id: 1,
@@ -11,13 +25,13 @@ export const pricingPlans = [
       "Projects and payments management*",
       "Lossless audio streaming",
       "Free service updates and access to email support",
-      "Up to 256GB Storage Included"
+      "Up to 256GB Storage Included",
     ],
     extras: [
       "$7/mo for every additional 256GB of storage added to your monthly plan",
-      "No hidden fees! Upgrade storage as needed while keeping all other services unlimited"
+      "No hidden fees! Upgrade storage as needed while keeping all other services unlimited",
     ],
-    link: "#"
+    link: "#",
   },
   {
     id: 2,
@@ -29,11 +43,11 @@ export const pricingPlans = [
       "Host your own Projects and Clients",
       "Build from our most current source code",
       "Add your own storage and services",
-      "Not managed by Sonex"
+      "Not managed by Sonex",
     ],
     extras: null,
-    link: "#"
-  }
+    link: "#",
+  },
 ];
 
 export const features = [
@@ -42,31 +56,70 @@ export const features = [
     title: "All-in-One Platform",
     points: [
       "Store your audio sessions, handle payments, and communicate all in one place",
-      "Unlimited Projects, Unlimited Clients—no extra cost."
-    ]
+      "Unlimited Projects, Unlimited Clients—no extra cost.",
+    ],
   },
   {
     id: 2,
     title: "Seamless Client Collaboration",
     points: [
       "Clients can upload files, request revisions, and pay—without needing to log in",
-      "Centralized revisions and payment tracking"
-    ]
+      "Centralized revisions and payment tracking",
+    ],
   },
   {
     id: 3,
     title: "Open-Source Flexibility",
     points: [
       "Access and customize Sonex's code to build features that fit your needs",
-      "Built by developers and audio pros, maintained by a passionate community"
-    ]
+      "Built by developers and audio pros, maintained by a passionate community",
+    ],
   },
   {
     id: 4,
     title: "No Lost Revisions, No Late Payments",
     points: [
       "Keep client revisions organized in one place",
-      "Files are locked behind your custom paywall, released upon final payment"
-    ]
-  }
+      "Files are locked behind your custom paywall, released upon final payment",
+    ],
+  },
+];
+
+export const users = [
+  {
+    id: "engineer",
+    tabTitle: "an Audio Engineer",
+    header: "Audio Engineers",
+    contentTitle: "Manage your clients and sessions",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui at beatae veritatis, dignissimos, rem illum a explicabo excepturi minus provident iusto ipsam omnis, praesentium nostrum.",
+    images: ["https://placehold.co/1200x900"],
+  },
+  {
+    id: "producer",
+    tabTitle: "a Music Producer",
+    header: "Music Producers",
+    contentTitle: "Store and Host Your Beats",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui at beatae veritatis, dignissimos, rem illum a explicabo excepturi minus provident iusto ipsam omnis, praesentium nostrum.",
+    images: ["https://placehold.co/1200x900"],
+  },
+  {
+    id: "podcast",
+    tabTitle: "a Podcast Editor",
+    header: "Podcast Editors",
+    contentTitle: "Showcase your podcast editing skills",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui at beatae veritatis, dignissimos, rem illum a explicabo excepturi minus provident iusto ipsam omnis, praesentium nostrum.",
+    images: ["https://placehold.co/1200x900"],
+  },
+  {
+    id: "songwriter",
+    tabTitle: "a Songwriter",
+    header: "Songwriters",
+    contentTitle: "Write, then sell your references",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui at beatae veritatis, dignissimos, rem illum a explicabo excepturi minus provident iusto ipsam omnis, praesentium nostrum.",
+    images: ["https://placehold.co/1200x900"],
+  },
 ];
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import { Card, CardBody, CardHeader, Tooltip } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Tab,
+  Tabs,
+  Tooltip,
+} from "@nextui-org/react";
 import FancyButton from "../components/FancyButton";
 import Header from "../sections/Header";
 import PricingPlan from "../components/PricingPlan";
 import Features from "../sections/Features";
+import { useState } from "react";
+import { users } from "../constants";
 
 const Home = () => {
+  const [selected, setSelected] = useState("engineer");
   return (
     <div>
       <Header />
@@ -23,10 +33,47 @@ const Home = () => {
       </section>
 
       {/* Features */}
-      <h2>Why Sonex?</h2>
-      <section className="flex">
-        <Features />
+      <section>
+        <h2>Why Sonex?</h2>
+        <div className="flex">
+          <Features />
+        </div>
       </section>
+
+      {/* Ways to use sonex */}
+      <section className="h-screen">
+        <h2 className="text-5xl font-bold">Ways you can use Sonex</h2>
+        <div className="my-8">
+          <p>Use Sonex as:</p>
+          <Tabs items={users} variant="bordered" isVertical>
+            {(usr) => (
+              <Tab key={usr.id} title={usr.tabTitle}>
+                <div className="ml-10">
+                  <h2 className="text-2xl font-extrabold">
+                    Built for {usr.header}
+                  </h2>
+                  <div className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <h4 className="text-lg">{usr.contentTitle}</h4>
+                      <p>{usr.content}</p>
+                    </div>
+                    <div className="col-span-3">
+                      {usr.images.map((imgLink) => (
+                        <img
+                          src={imgLink}
+                          alt={`Sonex works for ${usr.header}`}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </Tab>
+            )}
+          </Tabs>
+        </div>
+      </section>
+
+      {/* Pricing */}
       <section>
         <h3>Pricing</h3>
         <div className="max-w-4xl mx-auto">
