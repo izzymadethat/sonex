6e6d14cf6146590965b2ac7d5b8d3223375fd264
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-10-02 08:17:24 -0400

    create notification card

---
+++ frontend/src/components/NotificationCard.jsx
@@ -0,0 +1,19 @@
+import "../styles/notification-card.css";
+
+const NotificationCard = ({ notification }) => {
+  return (
+    <div class="notification">
+      <div class="notiglow"></div>
+      <div class="notiborderglow"></div>
+      <div class="notititle">{notification.title}</div>
+      <div className="z-10">
+        <p className="text-[9px] text-[#32a6ff] px-5 hover:translate-x-1 duration-300 transition-all">
+          {notification.author} | {notification.date}
+        </p>
+        <div class="notibody">{notification.message}</div>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationCard;
---
+++ frontend/src/constants/notifications.js
@@ -0,0 +1,20 @@
+export const notifications = [
+  {
+    id: 1,
+    title: "Welcome to version 1.0.0 Beta!",
+    message: `As the first release of Sonex, we're excited to introduce
+    you to the barebones of our platform. We're constantly working to improve and expand Sonex, and we're excited to hear your feedback. Please use the feedback/bug report form to let us know what you think.`,
+    author: "Sonex Team",
+    date: new Date().toLocaleDateString(),
+    linkText1: "View details and projected updates"
+  },
+  {
+    id: 2,
+    title: "Your contributions are appreciated!",
+    message: `We're looking to provide an all in one solution for audio professionals of all kinds. We believe that audio freelancing should be seamless. If you're interested in our mission, please consider making a contribution. Thank you!`,
+    author: "Sonex Team",
+    date: new Date().toLocaleDateString(),
+    linkText1: "View details and projected updates",
+    linkText2: "Make a contribution"
+  }
+];
