931a83a3af2dd4b2fce99869044a07274f8246a0
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-11-04 11:26:39 -0500

    add client view and update project share link

--- frontend/src/pages/user/projects/ViewSingleProjectPage.jsx
+++ frontend/src/pages/user/projects/ViewSingleProjectPage.jsx
@@ -10,7 +10,7 @@ import {
   convertStorageInMBtoGB
 } from "@/helper/equations";
 import FileTable from "./FileTable";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import EditProjectForm from "@/components/popups/EditProjectForm";
 import CopyProjectLink from "@/components/popups/CopyProjectLink";
@@ -29,19 +29,26 @@ import {
 } from "@/features/files/filesSlice";
 import { fetchCommentsByProject } from "@/features/comments/commentsSlice";
 import Loader from "@/components/informational/Loader/Loader";
+import { restoreUser, selectUser } from "@/features/user/userSlice";
+import { Button } from "@/components/ui/button";
 
 const ViewSingleProjectPage = () => {
   const params = useParams();
   const { projectId } = params;
+  const [searchParams] = useSearchParams();
+  const isClientView = searchParams.get("client_view") === "true";
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const project = useSelector(selectCurrentProject);
   const files = useSelector(selectProjectFiles);
   const [loading, setLoading] = useState(true);
   const [totalStorageUsed, setTotalStorageUsed] = useState(0);
+  const { currentUser: user } = useSelector(selectUser);
 
   useEffect(() => {
     setLoading(true);
     const restoreAccountInfo = async () => {
+      dispatch(restoreUser());
       // Fetch the project files, comments, project details and files
       dispatch(getProjects());
       dispatch(getSingleProject(projectId)).then(() => {
@@ -50,9 +57,7 @@ const ViewSingleProjectPage = () => {
       });
     };
 
-    restoreAccountInfo();
-
-    setLoading(false);
+    restoreAccountInfo().finally(() => setLoading(false));
   }, [dispatch, projectId]);
 
   // Every time a file is uploaded, recalculate the total storage used
@@ -60,11 +65,6 @@ const ViewSingleProjectPage = () => {
     const totalSize = files.reduce((acc, file) => acc + file.size, 0);
     setTotalStorageUsed(convertFileSizeInBytestoMB(totalSize));
   }, [files]);
-
-  // const updateTotalStorage = (files) => {
-  //   const totalSize = files.reduce((acc, file) => acc + file.size, 0);
-  //   setTotalStorageUsed(convertFileSizeInBytestoMB(totalSize));
-  // };
 
   if (loading || !project) {
     return (
@@ -74,56 +74,83 @@ const ViewSingleProjectPage = () => {
     );
   }
 
+  // Determine if the current user is the project owner
+  const isOwner = project.userId === user._id;
+
   return (
     <section className="m-8 space-y-8">
-      {/* Project Details */}
-      <div className="grid items-center justify-between grid-cols-1 space-y-4 lg:space-y-0 lg:grid-cols-5">
-        <div className="flex flex-col items-center justify-center col-span-3 gap-4 lg:items-start">
-          <NavigateBackTo route="/user/me/projects" pageName="projects" />
-          <div className="flex gap-2">
-            <h1 className="text-2xl font-bold">{project.title}</h1>
-            <ProjectStatusBadge projectStatus={project.status} />
-            <PaymentStatusBadge paymentStatus={project.paymentStatus} />
-          </div>
-          <p className="max-w-xs text-sm text-muted-foreground">
-            {project.description}
-          </p>
+      {isClientView && (
+        <div>
+          <h1 className="text-2xl font-bold uppercase">{project.title}</h1>
+          <p className="italic">created by {user.username}</p>
+          {isOwner && (
+            <Button
+              className="mt-4"
+              onClick={() => {
+                navigate(`/user/me/projects/${project._id}`);
+              }}
+            >
+              View project as creator
+            </Button>
+          )}
         </div>
+      )}
 
-        {/* Project Buttons */}
-        <div className="flex items-center gap-4">
-          <EditProjectForm project={project} />
-          <CopyProjectLink project={project} />
+      {/* Project Details */}
+      {isOwner && !isClientView && (
+        <div className="grid items-center justify-between grid-cols-1 space-y-4 lg:space-y-0 lg:grid-cols-5">
+          <div className="flex flex-col items-center justify-center col-span-3 gap-4 lg:items-start">
+            <NavigateBackTo route="/user/me/projects" pageName="projects" />
+            <div className="flex gap-2">
+              <h1 className="text-2xl font-bold">{project.title}</h1>
+              <ProjectStatusBadge projectStatus={project.status} />
+              <PaymentStatusBadge paymentStatus={project.paymentStatus} />
+            </div>
+            <p className="max-w-xs text-sm text-muted-foreground">
+              {project.description}
+            </p>
+          </div>
+
+          {/* Project Buttons */}
+          <div className="flex items-center gap-4">
+            <EditProjectForm project={project} />
+            <CopyProjectLink project={project} />
+          </div>
         </div>
-      </div>
+      )}
 
       {/* Project Overview and Stats */}
-      <div className="grid items-start grid-cols-1 gap-4 lg:grid-cols-2">
-        <Card>
-          <CardHeader>
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-lg lg:text-xl">
-                Project Balance
-              </CardTitle>
-              <CardDescription className="text-lg font-bold text-muted-foreground">
-                {project.paymentStatus === "no-charge"
-                  ? `No Charge`
-                  : `$${project.projectAmount.toFixed(2)}`}
-              </CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card>
-          <CardHeader>
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-lg lg:text-xl">Storage Used</CardTitle>
-              <CardDescription className="text-lg font-bold text-muted-foreground">
-                {convertStorageInMBtoGB(totalStorageUsed).toFixed(2)}Gb / 256Gb
-              </CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-      </div>
+      {isOwner && !isClientView && (
+        <div className="grid items-start grid-cols-1 gap-4 lg:grid-cols-2">
+          <Card>
+            <CardHeader>
+              <div className="flex items-center justify-between">
+                <CardTitle className="text-lg lg:text-xl">
+                  Project Balance
+                </CardTitle>
+                <CardDescription className="text-lg font-bold text-muted-foreground">
+                  {project.paymentStatus === "no-charge"
+                    ? `No Charge`
+                    : `$${project.projectAmount.toFixed(2)}`}
+                </CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <div className="flex items-center justify-between">
+                <CardTitle className="text-lg lg:text-xl">
+                  Storage Used
+                </CardTitle>
+                <CardDescription className="text-lg font-bold text-muted-foreground">
+                  {convertStorageInMBtoGB(totalStorageUsed).toFixed(2)}Gb /
+                  256Gb
+                </CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+        </div>
+      )}
 
       {/* TODO: Add Clients View (Later update) */}
       <FileTable projectId={params.projectId} />
